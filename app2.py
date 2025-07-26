acd7eed9815900b8b633e240dbc3f0175fb62480
MARFU-Coder <hola.marfu@gmail.com>
2025-07-26 18:11:30 -0300

    Delete app2.py

--- app2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from datetime import datetime
-
-st.set_page_config(layout="wide")
-st.title("🚀 Diagnóstico Panadería Integral")
-st.success(f"App cargada correctamente - {datetime.now()}")
